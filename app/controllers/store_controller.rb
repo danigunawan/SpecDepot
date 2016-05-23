243973779a142a6fe058781e662968a272cb7c89
Alfie Mendoza <alfie.mendoza@adish.co.jp>
2016-05-23 08:06:37 +0800

    Update on search sql

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -13,8 +13,8 @@ class StoreController < ApplicationController
       end
     else
       params[:search_category] == 'category' ?
-      @products = Product.joins("LEFT JOIN categories on products.category_id == categories.id").where("name LIKE ?", "%#{params[:search_query]}%"):
-      @products = Product.joins("LEFT JOIN categories on products.category_id == categories.id").where("title LIKE ?", "%#{params[:search_query]}%")
+      @products = Product.joins("LEFT JOIN categories on products.category_id = categories.id").where("name LIKE ?", "%#{params[:search_query]}%"):
+      @products = Product.joins("LEFT JOIN categories on products.category_id = categories.id").where("title LIKE ?", "%#{params[:search_query]}%")
             
       @products = @products.order("LOWER(name)","LOWER(title)", "price").paginate(page: params[:page], per_page: 4)
     
